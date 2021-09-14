4f0d0dbff0a3717d8049eac45b6915c3d5c6e744
Michael Kharchenko <mkharchenko@deloitte.ua>
2021-09-14 15:47:40 +0300

    Add Project

---
+++ FlowersHub/FlowerHub.Infrustructure/SqlConfigurationHelper.cs
@@ -0,0 +1,21 @@
+﻿using System.IO;
+using Microsoft.Extensions.Configuration;
+
+namespace FlowerHub.Infrustructure
+{
+    public class SqlConfigurationHelper
+    {
+        private static readonly IConfigurationRoot _configurationRoot;
+        static SqlConfigurationHelper()
+        {
+            _configurationRoot = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", true, true)
+                .AddEnvironmentVariables()
+                .Build();
+        }
+
+        public static string ConnectionString() => _configurationRoot.GetConnectionString("ConnectionString");
+
+    }
+}
